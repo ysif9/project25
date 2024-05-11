e10b3560d45a1adc21afe09bee9399750fc3a072
dark-hunter0 <afarag70325@yahoo.com>
2024-05-11 17:26:39 +0300

    post cell inital

---
+++ src/main/java/com/project25/Controllers/PostCellController.java
@@ -0,0 +1,12 @@
+package com.project25.Controllers;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PostCellController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
